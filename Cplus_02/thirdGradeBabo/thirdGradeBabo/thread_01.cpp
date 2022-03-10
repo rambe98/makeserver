9abb1a1ac5b00062d209b9f03c2c294a21f9a8d2
DESKTOP-U1JMMFV\오규한 <rbgks33@gmail.com>
2022-03-10 15:51:49 +0900

    thread-예제  2주차 수업

---
+++ Cplus_02/thirdGradeBabo/thirdGradeBabo/thread_01.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int trigger;
+
+void seokHoon() {
+    while (trigger == 0) {
+        cout << "정석아 사랑해\n";
+        this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
+void JeongSeok() {
+    while (trigger == 0) {
+        this_thread::sleep_for(std::chrono::milliseconds(500));
+        cout << "석훈아 사랑해\n";
+        this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+int main()
+{
+    trigger = 0;
+    cout << "\n--그들의 사랑이 시작된다...\n\n";
+    thread na(seokHoon);
+    thread oh(JeongSeok);
+
+    while (trigger == 0) {
+        cin >> trigger;
+    }
+
+    na.join();
+    oh.join();
+    cout << "\n--그들의 사랑이 시작된다...\n\n";
+}
+
